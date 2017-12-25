bc450c87f1693c45c9813586fcaa688ac064150c
yangtt <15195760150@163.com>
2017-12-25 22:21:02 +0800

    add homepage

--- salarycalc/urls.py
+++ salarycalc/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import home
 
 urlpatterns = [
+    path('', home.home, name='home'),
     path('admin/', admin.site.urls),
     path('employees/', include('employees.urls')),
 ]
