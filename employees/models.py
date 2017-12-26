3f900759b2fec96d2d5c2fc16f88e3d7a0f19d76
yangtt <15195760150@163.com>
2017-12-26 22:50:02 +0800

    add formula

--- employees/models.py
+++ employees/models.py
@@ -7,9 +7,16 @@ class Employee(models.Model):
 
 
 class Product(models.Model):
+    WORK_TYPE_CHOICES = (
+        (0, 'count'),
+        (1, 'weight'),
+        (2, 'time')
+    )
     name = models.CharField(max_length=100)
     unit_price = models.FloatField()
     unit = models.FloatField(default=1)
+    work_type = models.IntegerField(choices=WORK_TYPE_CHOICES)
+
 
 class SalaryItem(models.Model):
     employee = models.ForeignKey(Employee)
@@ -20,11 +27,32 @@ class SalaryItem(models.Model):
 
     @property
     def price(self):
+        price={}
         unit_price = self.product.unit_price
         unit = self.product.unit
         item_price = self.complete*unit_price/unit
         if self.is_overtime:
             item_price *= OVERTIME_FACTOR
+        if self.product.work_type == 1:
+            formula = "{0} * {1} / {2} = {3}".format(
+                str(self.complete),
+                str(unit_price),
+                str(unit),
+                str(item_price))
+        else:
+            if unit == 1:
+                formula = "{0} * {1} = {2}".format(
+                    str(self.complete),
+                    str(unit_price),
+                    str(item_price))
+            else:
+                formula = "{0} * {1} / {2} = {3} {4}".format(
+                    str(self.complete),
+                    str(unit_price),
+                    str(unit),
+                    str(item_price),
+                    "Error! Please Check.")
+        price['formula'] = formula
+        price['result'] = item_price
         return item_price
-
 
